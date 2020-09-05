0ac5b5bfc6abfe8490000d8426f140cc22afe485
AC <ac@marketmining.com.br>
2020-09-05 16:50:01 -0300

    add aspect

---
+++ src/main/java/io/gitbut/acnaweb/springaop/aspect/AppAspect.java
@@ -0,0 +1,30 @@
+package io.gitbut.acnaweb.springaop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AppAspect {
+
+	// @Around -> advice
+	// pointcut -> métodos anotados com @LogExecutionTime
+	@Around("@annotation(LogExecutionTime)")
+	public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.currentTimeMillis();
+
+		// executa o método anotado
+		System.out.println("antes");
+		Object proceed = joinPoint.proceed();
+		System.out.println("depois");
+
+		long executionTime = System.currentTimeMillis() - start;
+
+		System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
+
+		return proceed;
+	}
+
+}
