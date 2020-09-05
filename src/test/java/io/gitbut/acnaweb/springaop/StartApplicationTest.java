f07749f3031d486dbbec5625d2b5a4ba2884fc1b
AC <ac@marketmining.com.br>
2020-09-05 16:02:31 -0300

    init

---
+++ src/test/java/io/gitbut/acnaweb/springaop/StartApplicationTest.java
@@ -0,0 +1,13 @@
+package io.gitbut.acnaweb.springaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
