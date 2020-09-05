3f403a7e484f46245d55020882f47dcb8757e139
AC <ac@marketmining.com.br>
2020-09-05 16:20:24 -0300

    adicionando aspecto

---
+++ src/main/java/io/gitbut/acnaweb/springaop/aspect/AdderAfterReturnAspect.java
@@ -0,0 +1,12 @@
+package io.gitbut.acnaweb.springaop.aspect;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdderAfterReturnAspect {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public void afterReturn(Object returnValue) throws Throwable {
+		logger.info("value return was {}", returnValue);
+	}
+}
